c1766c67f11b4f6aecac8113172a2a6397f1ceb4
krystiano13 <crystianotv@gmail.com>
2024-07-16 11:29:24 +0200

    add validations and relations to GroupMember model

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,2 +1,8 @@
 class GroupMember < ApplicationRecord
+    validates :role, presence: true
+    validates :group_id, presence: true
+    validates :user_id, presence: true
+
+    belongs_to :user
+    belongs_to :group
 end
