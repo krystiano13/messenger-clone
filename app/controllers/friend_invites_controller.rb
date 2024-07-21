ae0af5711c1ef63b8bd4afa4d73cb281a843d1fb
krystiano13 <crystianotv@gmail.com>
2024-07-21 21:03:36 +0200

    create api function for deleting invite and sending invite

--- app/controllers/friend_invites_controller.rb
+++ app/controllers/friend_invites_controller.rb
@@ -8,10 +8,34 @@ class FriendInvitesController < ApplicationController
     end
 
     def create
+        new_invite = FriendInvite.new(invite_params)
 
+        if new_invite.save!
+            return render json: {
+                :info => "Invite sent",
+                :invite => new_invite
+            }, status: :ok
+        else
+            return render json: {
+                :errors => new_invite.errors
+            }, status: :unprocessable_entity
+        end
     end
 
     def destroy
+        @invite = FriendInvite.find(params[:id])
 
+        if @invite.present?
+            @invite.destroy
+        end
+
+        return render json: {
+            :status => "deleted"
+        }, status: :ok
+    end
+
+    private
+    def invite_params
+        return params.permit(:user_id, :inviter_id)
     end
 end
