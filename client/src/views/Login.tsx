2f26d386a8b46cada4b47fa63406df64bd020840
krystiano13 <crystianotv@gmail.com>
2024-07-22 11:12:20 +0200

    create view for login

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,5 +1,43 @@
 export function Login() {
-    return (
-        <></>
-    )
+  return (
+    <div className="w-full h-full flex bg-gray-900 bg-opacity-50 justify-center items-center">
+      <section className="w-[90vw] md:w-96 p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+          Welcome Back !
+        </h2>
+
+        <form>
+          <div className="flex flex-col gap-6 mt-4">
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">
+                Email Address
+              </label>
+              <input
+                id="emailAddress"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-gray-700 dark:text-gray-200">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              />
+            </div>
+          </div>
+
+          <div className="flex mt-6">
+            <button className="px-8 py-2.5 w-full leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+              Log In
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
