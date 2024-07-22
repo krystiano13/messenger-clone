dcf51ab719f7a42249b461c5455c4b00c13aa23c
krystiano13 <crystianotv@gmail.com>
2024-07-22 10:58:35 +0200

    add some additional styling for mobile versions

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -5,17 +5,21 @@ import { FriendTab } from "../components/Home/FriendTab";
 export function Home() {
   return (
     <div className="w-full h-full flex justify-between">
-      <section className="w-1/3 flex flex-col gap-5 bg-gray-900 bg-opacity-25 p-4">
-        <div className="flex w-full justify-start items-center gap-4">
-          <HamburgerButton />
-          <h1 className="text-center text-white text-2xl">Chats</h1>
+      <section className="w-full md:w-1/3 flex flex-col gap-5 bg-gray-900 bg-opacity-25 p-4">
+        <div className="flex flex-col gap-5 sticky">
+          <div className="flex w-full justify-start items-center gap-4">
+            <HamburgerButton />
+            <h1 className="text-center text-white text-2xl">Chats</h1>
+          </div>
+          <Searchbar />
         </div>
-        <Searchbar />
         <div id="chats" className="h-fit flex-1 overflow-y-auto">
           <FriendTab />
         </div>
       </section>
-      <section className="w-2/3"></section>
+      <section className="hidden md:w-2/3 md:flex items-center justify-center">
+        <h1 className="text-white font-medium text-5xl">Messenger clone</h1>
+      </section>
     </div>
   );
 }
