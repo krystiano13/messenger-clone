e2836cdaf2c030ed464ecad6423ca4eab938037b
krystiano13 <crystianotv@gmail.com>
2024-07-22 10:42:41 +0200

    move searchbar and hamburger button into separate component

---
+++ client/src/components/Home/HamburgerButton.tsx
@@ -0,0 +1,9 @@
+export function HamburgerButton() {
+  return (
+    <button className="flex flex-col justify-center w-6 h-6 gap-[0.3rem]">
+      <span className="w-full h-[2px] bg-white"></span>
+      <span className="w-full h-[2px] bg-white"></span>
+      <span className="w-full h-[2px] bg-white"></span>
+    </button>
+  );
+}
