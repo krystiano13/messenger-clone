7a6ce7b88735cdaba1956b294e432ab77216bae5
krystiano13 <crystianotv@gmail.com>
2024-07-22 11:00:11 +0200

    add react-router to the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
-import { Home } from "./views/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./views/Home";
 
 function App() {
   return (
     <div className="w-[100vw] h-[100vh] bg-gray-800 overflow-x-hidden">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
