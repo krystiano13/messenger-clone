f0e47ced4db875b20401a9c423f5e8a048bda04b
krystiano13 <crystianotv@gmail.com>
2024-07-22 12:20:06 +0200

    add entry animation in Home view

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,24 +1,64 @@
+import { motion } from "framer-motion";
+
 import { HamburgerButton } from "../components/Home/HamburgerButton";
 import { Searchbar } from "../components/Home/Searchbar";
 import { FriendTab } from "../components/Home/FriendTab";
 
 export function Home() {
   return (
-    <div className="w-full h-full flex justify-between">
+    <div className="w-full h-full flex justify-between overflow-hidden">
       <section className="w-full md:w-1/3 flex flex-col gap-5 bg-gray-900 bg-opacity-25 p-4">
-        <div className="flex flex-col gap-5 sticky">
+        <motion.div
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            bounce: 0.5,
+          }}
+          animate={{
+            opacity: [0, 1],
+            y: [30, 0],
+          }}
+          className="flex flex-col gap-5 sticky"
+        >
           <div className="flex w-full justify-start items-center gap-4">
             <HamburgerButton />
             <h1 className="text-center text-white text-2xl">Chats</h1>
           </div>
           <Searchbar />
-        </div>
-        <div id="chats" className="h-fit flex-1 overflow-y-auto">
+        </motion.div>
+        <motion.div
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            bounce: 0.5,
+            delay: 0.2,
+          }}
+          animate={{
+            opacity: [0, 1],
+            y: [30, 0],
+          }}
+          id="chats"
+          className="h-fit flex-1 overflow-y-auto"
+        >
           <FriendTab />
-        </div>
+        </motion.div>
       </section>
       <section className="hidden md:w-2/3 md:flex items-center justify-center">
-        <h1 className="text-white font-medium text-5xl">Messenger clone</h1>
+        <motion.h1
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            bounce: 0.5,
+            delay: 0.35,
+          }}
+          animate={{
+            opacity: [0, 1],
+            y: [30, 0],
+          }}
+          className="text-white font-medium text-5xl"
+        >
+          Messenger clone
+        </motion.h1>
       </section>
     </div>
   );
