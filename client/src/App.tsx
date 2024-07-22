ff204073c5224e809ec1111531b12adc218fb3f0
krystiano13 <crystianotv@gmail.com>
2024-07-22 10:39:39 +0200

    create html and styles for search section in homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import { Home } from "./views/Home"
+
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div className="w-[100vw] h-[100vh] bg-gray-800 overflow-x-hidden">
+      <Home />
+    </div>
+  );
 }
 
 export default App
