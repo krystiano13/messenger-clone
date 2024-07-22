6a9db7acadd2750ac462370b06d70b4743dcc122
krystiano13 <crystianotv@gmail.com>
2024-07-22 11:07:12 +0200

    create register view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./views/Home";
+import { Login } from "./views/Login";
+import { Register } from "./views/Register";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
