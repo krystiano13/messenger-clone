664d7b04a6c7489fd55dfed7eebcc1afad470c3d
krystiano13 <crystianotv@gmail.com>
2024-07-24 12:16:36 +0200

    create head section of chat (that contains name of our friend)

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,7 +1,11 @@
 export function Chat() {
-    return (
-        <div className="w-full h-full">
-
-        </div>
-    )
+  return (
+    <div className="w-full h-full max-h-[100vh] overflow-y-auto flex flex-col justify-start">
+      <section className="flex justify-center items-center p-4 shadow-md">
+        <h2 className="text-white text-center text-xl font-regular">
+          John Doe
+        </h2>
+      </section>
+    </div>
+  );
 }
