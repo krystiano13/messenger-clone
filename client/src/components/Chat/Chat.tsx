51e184ad537ccecdc838537670ee053717b358a3
krystiano13 <crystianotv@gmail.com>
2024-07-24 12:44:55 +0200

    implement button for closing chat in mobile mode

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -3,7 +3,10 @@ import { Message } from "./Message";
 export function Chat() {
   return (
     <div className="w-full h-full max-h-[100vh] overflow-y-auto flex flex-col bg-slate-800 justify-start p-1">
-      <section className="flex justify-center items-center p-4 shadow-md">
+      <section className="flex justify-center items-center p-4 shadow-md gap-4">
+        <button className="md:hidden px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+          Close
+        </button>
         <h2 className="text-white text-center text-xl font-regular">
           John Doe
         </h2>
