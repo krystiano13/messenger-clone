a9787d483f068169b1a9b044f840dc660be8a87e
krystiano13 <crystianotv@gmail.com>
2024-07-26 14:04:07 +0200

    create 2 buttons for switichin modes in Friends view

--- client/src/views/Friends.tsx
+++ client/src/views/Friends.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Searchbar } from "../components/Home/Searchbar";
 import { useNavigate } from "react-router";
 import { FriendSearchTab } from "../components/Friends/FriendSearchTab";
 
 export default function Friends() {
+  const [findFriends, setFindFriend] = useState<boolean>(false);
   const navigate = useNavigate();
   return (
     <div className="w-full h-full flex flex-col items-center p-6 gap-6">
@@ -15,6 +17,28 @@ export default function Friends() {
         </button>
       </div>
       <Searchbar search={(value: string) => {}} />
+      <section className="flex items-center gap-3 w-full">
+        <button
+          onClick={() => setFindFriend(false)}
+          className={`px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform ${
+            findFriends && "bg-gray-700 rounded-md hover:bg-gray-600"
+          } ${
+            !findFriends && "bg-blue-700 rounded-md hover:bg-blue-600"
+          } focus:outline-none`}
+        >
+          Friends
+        </button>
+        <button
+          onClick={() => setFindFriend(true)}
+          className={`px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform ${
+            !findFriends && "bg-gray-700 rounded-md hover:bg-gray-600"
+          } ${
+            findFriends && "bg-blue-700 rounded-md hover:bg-blue-600"
+          } focus:outline-none`}
+        >
+          Find new friend
+        </button>
+      </section>
       <div className="w-full flex flex-col items-center justify-start">
         <FriendSearchTab name="John Doe" />
       </div>
